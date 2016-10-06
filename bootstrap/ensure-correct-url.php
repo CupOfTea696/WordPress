92a95014c0ead43e6910ef9999d1bc703dc6bc2e
Sven Wittevrongel <cupoftea696@gmail.com>
2016-10-06 14:27:29 +0100

    Improved bootstrap

---
+++ bootstrap/ensure-correct-url.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Ensure Correct URL
+ */
+
+if (! is_blog_installed()) {
+    return;
+}
+
+$__ensureCorrectBackendUrl = function ()
+{
+    $siteurl = get_option('siteurl');
+    
+    if (! preg_match('/\\/wp\\/?$/', $siteurl)) {
+        update_option('siteurl', $siteurl . (preg_match('/\\/$/', $siteurl) ? 'wp' : '/wp'));
+    }
+};
+
+$__ensureCorrectBackendUrl();
+
+$__ensureCorrectFrontendUrl = function ()
+{
+    $home = get_option('home');
+    
+    if (! preg_match('/\\/wp\\/?$/', $home)) {
+        update_option('home', preg_replace('/\\/wp(\\/?)$/', '$1', $home));
+    }
+};
+
+$__ensureCorrectFrontendUrl();
+
+unset($__ensurePreferredDomain, $__ensureCorrectBackendUrl);
--- bootstrap/set-theme-dir.php
+++ bootstrap/set-theme-dir.php
@@ -1,3 +1,10 @@
 <?php
+/**
+ * Plugin Name: Set Theme Directory
+ */
+
+if (! is_blog_installed()) {
+    return;
+}
 
 register_theme_directory(dirname(__DIR__) . '/' . env('APP_PUBLIC', 'public') . '/themes');
