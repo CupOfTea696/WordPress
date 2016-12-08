7d1fb29204b729345df1e6f72b8764ffadab214e
Sven Wittevrongel <cupoftea696@gmail.com>
2016-12-08 11:50:11 +0000

    Add meta Class

---
+++ WordPress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CupOfTea;
+
+use CupOfTea\Package\Package;
+use CupOfTea\Package\Contracts\Package as PackageContract;
+
+class WordPress implements PackageContract
+{
+    use Package;
+    
+    /**
+     * Package Vendor.
+     *
+     * @const string
+     */
+    const VENDOR = 'CupOfTea';
+    
+    /**
+     * Package Name.
+     *
+     * @const string
+     */
+    const PACKAGE = 'WordPress';
+    
+    /**
+     * Package Version.
+     *
+     * @const string
+     */
+    const VERSION = '0.0.0';
+}
