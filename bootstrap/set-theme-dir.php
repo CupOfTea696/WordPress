ae203fb6a38fb58a1a8ab87451e680d628f3a764
Sven Wittevrongel <cupoftea696@gmail.com>
2017-01-04 12:44:40 +0000

    Use public_path to register theme directory

--- bootstrap/set-theme-dir.php
+++ bootstrap/set-theme-dir.php
@@ -7,7 +7,7 @@ if (! is_blog_installed()) {
     return;
 }
 
-register_theme_directory(dirname(__DIR__) . '/' . env('APP_PUBLIC', 'public') . '/themes');
+register_theme_directory(public_path('themes'));
 
 add_filter('wp_prepare_themes_for_js', function ($themes) {
     foreach ($themes as &$theme) {
