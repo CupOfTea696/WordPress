44e809d8330f9a160bd1220a01cb0065af326485
Sven Wittevrongel <cupoftea696@gmail.com>
2017-01-04 12:41:29 +0000

    Fix theme screenshots

--- bootstrap/set-theme-dir.php
+++ bootstrap/set-theme-dir.php
@@ -8,3 +8,15 @@ if (! is_blog_installed()) {
 }
 
 register_theme_directory(dirname(__DIR__) . '/' . env('APP_PUBLIC', 'public') . '/themes');
+
+add_filter('wp_prepare_themes_for_js', function ($themes) {
+    foreach ($themes as &$theme) {
+        if (! empty($theme['screenshot'])) {
+            foreach ($theme['screenshot'] as &$screenshot) {
+                $screenshot = str_replace(public_path(), '', $screenshot);
+            }
+        }
+    }
+    
+    return $themes;
+});
