c2c98b676a6c105594c17ac2ae5ddb34f61635fd
Sven Wittevrongel <cupoftea696@gmail.com>
2017-02-07 13:26:20 +0000

    Let's just version these as well.

--- bootstrap/sanitize-file-names.php
+++ bootstrap/sanitize-file-names.php
@@ -3,11 +3,15 @@
  * Plugin Name: Sanitize File Names
  */
 
-add_filter('sanitize_file_name', function ($filename) {
-    $path = pathinfo($filename);
+add_filter('wp_handle_upload_prefilter', function($file) {
+    $version = file_exists($file['tmp_name']) ? '.' . mb_substr(md5_file($file['tmp_name']), 0, 8) : '';
+    
+    $path = pathinfo($file['name']);
     $basename = str_slug(preg_replace_callback('/[A-z]*[A-Z][A-z]*[A-Z][A-z]*/', function ($matches) {
         return snake_case($matches[0], '-');
-    }, $path['filename'])) . '.' . $path['extension'];
+    }, $path['filename'])) . $version . '.' . $path['extension'];
     
-    return str_replace($path['basename'], $basename, $filename);
+    $file['name'] = str_replace($path['basename'], $basename, $file['name']);
+    
+    return $file;
 });
