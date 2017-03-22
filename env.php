56f8097452f654cda034f6851d43f4aad354771e
CupOfTea696 <cupoftea696@gmail.com>
2017-03-22 18:04:35 +0000

    Add env.php file with instructions

---
+++ env.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Environment Detection
+ * 
+ * Use this file to write logic to detect which environment file
+ * the application should use. Select a file name by
+ * returning a string. Make sure to check if
+ * the file you want to use exists.
+ * 
+ * The base_path() helper is already available.
+ * 
+ * Example usage:
+ * 
+ * if (preg_match('/(^localhost|\\.local)$/', $host) && file_exists(base_path('.env.local'))) {
+ *     return '.env.local';
+ * }
+ * 
+ */
