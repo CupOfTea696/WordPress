f16b4d9833439d4949324e55bc6f6c866d26f360
CupOfTea696 <cupoftea696@gmail.com>
2017-05-17 10:13:00 +0100

    Improved sanitize file names plugin

--- bootstrap/sanitize-file-names.php
+++ bootstrap/sanitize-file-names.php
@@ -3,13 +3,21 @@
  * Plugin Name: Sanitize File Names
  */
 
+use Illuminate\Support\Str;
+
 add_filter('wp_handle_upload_prefilter', function($file) {
-    $version = file_exists($file['tmp_name']) ? '.' . mb_substr(md5_file($file['tmp_name']), 0, 8) : '';
+    $version = file_exists($file['tmp_name']) ? '.' . Str::substr(md5_file($file['tmp_name']), 0, 8) : '';
     
     $path = pathinfo($file['name']);
-    $basename = str_slug(preg_replace_callback('/[A-z]*[A-Z][A-z]*[A-Z][A-z]*/', function ($matches) {
-        return snake_case($matches[0], '-');
-    }, $path['filename'])) . $version . '.' . $path['extension'];
+    $basename = Str::slug(preg_replace_callback_array(
+        [
+            '/[A-Z]{2,}/' => function ($matches) {
+                return Str::lower($matches[0]);
+            },
+            '/[A-z]*[A-Z][A-z]*[A-Z][A-z]*/' => function ($matches) {
+                return Str::snake($matches[0], '-');
+            },
+        ], $path['filename'])) . $version . '.' . $path['extension'];
     
     $file['name'] = str_replace($path['basename'], $basename, $file['name']);
     
